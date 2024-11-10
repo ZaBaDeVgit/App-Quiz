3e40552c9f7fe0ba7c02d57363eef38c9b1013ba
ZaBaDeV <zabal_9@hotmail.com>
2024-11-10 18:21:14 +0100

    v.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: 'https://balazastudio.github.io/App-Quiz/', // Asegúrate de que coincida con tu repositorio en GitHub
+  clearScreen: false,
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 });
